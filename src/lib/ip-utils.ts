ff7207dae69f3ebf6011a6ad811c88d10c8c613c
Tanmay Patil <tanmay3.patil@ril.com>
2025-05-21 11:50:39 +0530

    FIX: Country name in Views

--- src/lib/ip-utils.ts
+++ src/lib/ip-utils.ts
@@ -7,7 +7,7 @@ ip2location.open("./DB26.BIN");
 export function getCountryFromIP(ip: string): string | null {
   try {
     const result = ip2location.getAll(ip);
-    return result.country_short || null;
+    return result.countryLong || null;
   } catch (err) {
     console.error("IP2Location lookup failed:", err);
     return null;
